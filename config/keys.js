6930b90cd4d405e214c78526d837513cc4655fa6
kevin_xujiantong <314783087@qq.com>
2019-09-29 17:30:49 +0800

    mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI:"mongodb://xujiantong:123456A@ds151416.mlab.com:51416/ms4node"
+}
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to mongoDB
+mongoose.connect(db)
+		.then(()=>{
+			console.log("MongoDB Connected");
+		})
+		.catch((err)=>{
+			console.log(err);
+		})
 
 app.get("", (req, res)=>{
 	res.send("hello world2");
