b3c4a221f65845d68a5e0a7c860c3e2a9157f391
xujiantong <sddzxjt1688@gmail.com>
2019-10-01 02:55:40 +0800

    搭建注册接口并存储数据

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 // @login & register
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
+const User = require("../../models/User");
 // $route GET api/users/test
 // @desc 返回的请求的json数据
 // @access public
@@ -11,4 +13,35 @@ router.get("/test.json",(req, res)=>{
    })
 });
 
+// $route POST api/users/register
+// @desc 返回的请求的json数据
+// @access private
+router.post("/register",(req, res)=>{
+    // console.log(req.body);
+    // 查询数据库中是否拥有邮箱
+    User.findOne({email: req.body.email})
+        .then((user)=>{
+            if(user){
+                return res.status(400).json({email:"邮箱已被注册"});
+            }
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                // avatar,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                    if(err) throw err;
+
+                    newUser.password = hash;
+                    newUser.save()
+                        .then((user)=> res.json(user))
+                        .catch(err=> console.log(err));
+                });
+            });
+        })
+
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 // 引入users.js
 const users = require("./routes/api/users");
 // DB config
 const db = require("./config/keys").mongoURI;
+
+// 使用body-parser中间件
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Connect to mongoDB
 mongoose.connect(db)
